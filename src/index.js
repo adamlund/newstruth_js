f86a2d4f6d40928eabb9ccee3afd1f7c58194af7
Adam Lund <alund@mednetstudy.com>
2018-11-04 14:09:59 -0600

    Provide list of articles in json file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 const NewsTruth = require('./newstruth');
+const ARTICLES = require('./articles_list.json');
 
-const articles = [
-'https://www.cnn.com/2018/11/04/politics/georgia-voter-registration-hacking-attempt-investigation/index.html',
-// 'https://www.nytimes.com/2018/11/03/magazine/FBI-charlottesville-white-nationalism-far-right.html',
-// 'https://www.cnn.com/2018/11/02/opinions/nic-robertson-opinion-intl/index.html',
-// 'https://www.chron.com/news/article/Voters-set-to-render-fresh-verdict-on-Trump-13360929.php',
-];
+/**
+ * Modify implementation of getArticles() to change what articles are analyzed.
+ * @returns array of strings representing URLs of articles
+ */
+const getArticles = () => {
+    return ARTICLES.articles;
+};
 
 async function main() {
     const newsTruth = new NewsTruth();
     await newsTruth.init();
+    const articles = getArticles();
     // feed newstruth each article url, and it prints known liars to stdout
     for(const i in articles) {
         await newsTruth.analyze(articles[i]);
